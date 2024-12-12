31164e322dec3299859ac8a6023a8ff7c8c630eb
I050870 <nicolas.boucher@sap.com>
2024-12-12 19:33:54 +0100

    simplify config

--- wdio.default.conf.js
+++ wdio.default.conf.js
@@ -2,92 +2,23 @@
 const config = {
     baseURL: 'https://webdriver.io/',
 
-    // ==================
-    // Specify Test Files
-    // ==================
-    // Define which test specs should run. The pattern is relative to the directory
-    // from which `wdio` was called.
-    //
-    // If you are calling `wdio` from an NPM script (see https://docs.npmjs.com/cli/run-script),
-    // then the current working directory is where your `package.json` resides, so `wdio`
-    // will be called from there.
-    //
     specs: [
         './test.spec.js'
     ],
-    // define specific suites
-    suites: {},
-
-    exclude: [],
 
     sync: true,
     coloredLogs: true,
-
-    //
-    // Framework you want to run your specs with.
-    // The following are supported: 'mocha', 'jasmine', and 'cucumber'
-    // See also: https://webdriver.io/docs/frameworks.html
-    //
-    // Make sure you have the wdio adapter package for the specific framework installed before running any tests.
     framework: 'mocha',
     reporters: [
         'spec'
     ],
-
-    // Options to be passed to Mocha.
-    // See the full list at: http://mochajs.org
     mochaOpts: {
         ui: 'bdd'
     },
-    // ============
-    // Capabilities
-    // ============
-    // Define your capabilities here. WebdriverIO can run multiple capabilities at the same
-    // time. Depending on the number of capabilities, WebdriverIO launches several test
-    // sessions. Within your `capabilities`, you can overwrite the `spec` and `exclude`
-    // options in order to group specific specs to a specific capability.
-    //
-    // First, you can define how many instances should be started at the same time. Let's
-    // say you have 3 different capabilities (Chrome, Firefox, and Safari) and you have
-    // set `maxInstances` to 1. wdio will spawn 3 processes.
-    //
-    // Therefore, if you have 10 spec files and you set `maxInstances` to 10, all spec files
-    // will be tested at the same time and 30 processes will be spawned.
-    //
-    // The property basically handles how many capabilities from the same test should run tests.
-    //
-    maxInstancesPerCapability: 1,
     capabilities: [{
-        // maxInstances can get overwritten per capability. So if you have an in house Selenium
-        // grid with only 5 firefox instance available you can make sure that not more than
-        // 5 instance gets started at a time.
-        maxInstances: 1,
-        browserName: 'chrome',
-        'goog:chromeOptions': {
-            args: ['lang=en',
-                'window-size=1800,1100'],
-            prefs: {
-                'intl.accept_languages': 'en,EN'
-            }
-        }
+        browserName: 'chrome'
     }],
-    // ===================
-    // Test Configurations
-    // ===================
-    // Define all options that are relevant for the WebdriverIO instance here
-    //
-    // Level of logging verbosity: trace | debug | info | warn | error | silent
     logLevel: 'info',
-    //
-    // Set specific log levels per logger
-    // loggers:
-    // - webdriver, webdriverio
-    // - @wdio/applitools-service, @wdio/browserstack-service, @wdio/devtools-service, @wdio/sauce-service
-    // - @wdio/mocha-framework, @wdio/jasmine-framework
-    // - @wdio/local-runner, @wdio/lambda-runner
-    // - @wdio/sumologic-reporter
-    // - @wdio/cli, @wdio/config, @wdio/sync, @wdio/utils
-    // use 'silent' level to disable logger
     logLevels: {
         webdriver: 'warn'
     }
